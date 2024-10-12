1ca6d78d19891fd3390241fd1d6ce0c3d4cf3eb7
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2024-10-12 14:42:01 +0200

    update nvim config and plugins

--- lua/custom/plugins/debugging.lua
+++ lua/custom/plugins/debugging.lua
@@ -73,6 +73,21 @@ local dap_plugin = {
     vim.keymap.set('n', '<F9>', dap.step_over)
     vim.keymap.set('n', '<F10>', dap.step_into)
     vim.keymap.set('n', '<F11>', dap.step_out)
+
+    dap.adapters.php = {
+      type = "executable",
+      command = "node",
+      args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" }
+    }
+
+    dap.configurations.php = {
+        {
+            type = "php",
+            request = "launch",
+            name = "Listen for Xdebug",
+            port = 9003
+        }
+    }
   end,
 }
 
