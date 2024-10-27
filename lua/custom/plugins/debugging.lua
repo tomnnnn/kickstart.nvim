13fe9083f9e499991f9983983bdd53a8414d92a7
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2024-10-27 12:40:34 +0100

    simplify debug plugin config

--- lua/custom/plugins/debugging.lua
+++ lua/custom/plugins/debugging.lua
@@ -1,68 +1,50 @@
 local mason_dap = {
-  "jay-babu/mason-nvim-dap.nvim",
-  event = "VeryLazy",
+  'jay-babu/mason-nvim-dap.nvim',
+  event = 'VeryLazy',
   dependencies = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-dap",
+    'williamboman/mason.nvim',
+    'mfussenegger/nvim-dap',
   },
   opts = {
     handlers = {},
-  }
+  },
 }
 
 local dap_plugin = {
-  "mfussenegger/nvim-dap",
+  'mfussenegger/nvim-dap',
   config = function()
-    local dap = require("dap")
+    local dap = require 'dap'
 
-    vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-    vim.keymap.set('n', '<Leader>dc', dap.clear_breakpoints, { desc = "Clear breakpoints" })
-    vim.keymap.set('n', '<Leader>dr', dap.continue, { desc = "Run debugger" })
+    vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, { desc = 'Toggle breakpoint' })
+    vim.keymap.set('n', '<Leader>dc', dap.clear_breakpoints, { desc = 'Clear breakpoints' })
+    vim.keymap.set('n', '<Leader>dr', dap.continue, { desc = 'Run debugger' })
     vim.keymap.set('n', '<F8>', dap.continue)
     vim.keymap.set('n', '<F9>', dap.step_over)
     vim.keymap.set('n', '<F10>', dap.step_into)
     vim.keymap.set('n', '<F11>', dap.step_out)
-
-    dap.adapters.php = {
-      type = "executable",
-      command = "node",
-      args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" }
-    }
-
-    dap.configurations = {
-      {
-        php = {
-            type = "php",
-            request = "launch",
-            name = "Listen for Xdebug",
-            port = 9003
-        },
-    }
-  }
   end,
 }
 
 local dap_lldb = {
-   "julianolf/nvim-dap-lldb",
-   config = function()
-      local dap_lldb = require("dap-lldb")
-      local mason_registry = require("mason-registry")
-      local codelldb = mason_registry.get_package("codelldb")
-      dap_lldb.setup()
-   end,
-   dependencies = { "mfussenegger/nvim-dap" },
+  'julianolf/nvim-dap-lldb',
+  config = function()
+    require('dap-lldb').opts = { codelldb_path = require('mason-registry').get_package('codelldb'):get_install_path() }
+
+    require('dap-lldb').setup()
+  end;
+  dependencies = { 'mfussenegger/nvim-dap' },
 }
 
 local dap_ui = {
-  "rcarriga/nvim-dap-ui",
-  event = "VeryLazy",
+  'rcarriga/nvim-dap-ui',
+  event = 'VeryLazy',
   dependencies = {
-    "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio"
+    'mfussenegger/nvim-dap',
+    'nvim-neotest/nvim-nio',
   },
   config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
+    local dap = require 'dap'
+    local dapui = require 'dapui'
 
     dapui.setup()
 
@@ -85,5 +67,5 @@ return {
   mason_dap,
   dap_plugin,
   dap_ui,
-  dap_lldb
+  dap_lldb,
 }
